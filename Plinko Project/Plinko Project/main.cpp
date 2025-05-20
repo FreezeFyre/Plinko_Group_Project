910a9e1a07139633310c482d6e5dd5bab974adee
FreezeFyre <92956453+FreezeFyre@users.noreply.github.com>
2025-05-20 15:33:59 -0700

    v5

--- Plinko Project/Plinko Project/main.cpp
+++ Plinko Project/Plinko Project/main.cpp
@@ -29,22 +29,16 @@ struct ball_parameters {
     float rad;
 };
 // Set parameters for the ball and bind to "ball" name
-ball_parameters ball = {{sim_width/2,sim_height},{0.05,0},.8,0.5};
-
-std::unordered_map<int, ball_parameters> balls;
+std::unordered_map<int, ball_parameters> ball;
 void initialize_balls() {
-    balls[0] = {{0,0},{0,0},1,.5};
+    ball[0] = {{0,0},{0,0},1,.5};
 }
 
-// Declare Globale parameters
-struct global_parameters {
-    float gravity; // Gravity Force, "-" = down
-    float air_damping; // Energy loss every sample, 1 = no loss, 0 = total loss
-    int substeps; // Unused
-    int loop_thresh; // Max Amount of allowed samples
-};
-// Set Global parameters and bind to "global" name
-global_parameters global = {-9.8,.99,4,100};
+
+float gravity = -9.8; // Gravity Force, "-" = down
+float air_damping = .99; // Energy loss every sample, 1 = no loss, 0 = total loss
+int substeps = 100; // Unused
+int loop_thresh = 100; // Max Amount of allowed samples
 
 
 
@@ -63,99 +57,118 @@ void initialize_pins() {
 }
 
 
+void pin_collisions() {
+    int n = 0;
+    while (n < ball.size()) {
+        int i = 0;
+        while (i < pin.size()) {
+            float d = sqrt((pow((ball[n].pos[0] - pin[i].pos[0]), 2)) + (pow((ball[n].pos[1] - pin[i].pos[1]), 2)));
+            
+            if (d < pin[i].rad + ball[n].rad) { // Detect If Ball Collides With Pin i
+                std::cout << "X"; // Visulize Collision Detection
+                
+                float x_direction = ball[n].pos[0] - pin[i].pos[0]; // Vector Pointing From the Pin to the Ball
+                float y_direction = ball[n].pos[1] - pin[i].pos[1]; // Vector Pointing From the Pin to the Ball
+                
+                float normal_x = x_direction / d; // Normalize the Direction Vector
+                float normal_y = y_direction / d; // Normalize the Direction Vector
+                            
+                float dot = ball[n].velocity[0]*(normal_x * -1) + ball[n].velocity[1]*(normal_y * -1); // Dot Product
 
-void pin_solver() {
-    int i = 0;
-    while (i < pin.size()) { // Loop Through Each Pin
-        float d = sqrt((pow((ball.pos[0]-pin[i].pos[0]), 2)) + (pow((ball.pos[1] - pin[i].pos[1]), 2))); // Find Distance Between Pin i and the Ball
-        
-        if (d < pin[i].rad + ball.rad) { // Detect If Ball Collides With Pin i
-            std::cout << "X"; // Visulize Collision Detection
+                ball[n].velocity[0] = ball[n].velocity[0] - 2 * dot * normal_x; // Reflection Velocity
+                ball[n].velocity[1] = ball[n].velocity[1] - 2 * dot * normal_y; // Reflection Velocity
+                
+                
+                ball[n].pos[0] = pin[i].pos[0] + x_direction;
+                ball[n].pos[1] = pin[i].pos[1] + y_direction;
+                
+                ball[n].velocity[0] *= ball[n].bounce_damp; // Dampening
+                ball[n].velocity[1] *= ball[n].bounce_damp; // Dampening
+            }
             
-            float x_direction = ball.pos[0] - pin[i].pos[0]; // Vector Pointing From the Pin to the Ball
-            float y_direction = ball.pos[1] - pin[i].pos[1]; // Vector Pointing From the Pin to the Ball
-            
-            float normal_x = x_direction / d; // Normalize the Direction Vector
-            float normal_y = y_direction / d; // Normalize the Direction Vector
-                        
-            float dot = ball.velocity[0]*(normal_x * -1) + ball.velocity[1]*(normal_y * -1); // Dot Product
-
-            ball.velocity[0] = ball.velocity[0] - 2 * dot * normal_x; // Reflection Velocity
-            ball.velocity[1] = ball.velocity[1] - 2 * dot * normal_y; // Reflection Velocity
-            
-            
-            ball.pos[0] = pin[i].pos[0] + x_direction;
-            ball.pos[1] = pin[i].pos[1] + y_direction;
-            
-            ball.velocity[0] *= ball.bounce_damp; // Dampening
-            ball.velocity[1] *= ball.bounce_damp; // Dampening
+            i += 1;
         }
         
-        i += 1;
+        n += 1;
     }
 }
 
 
 
 void floor_ceil_collision(int floor_height, int ceil_height) {
-    // Detect Collision with floor
-    if (ball.pos[1] < floor_height) {
-        ball.velocity[1] *= -1; // Reflect Vertically
-        ball.pos[1] = floor_height + .01;
-        ball.velocity[0] *= ball.bounce_damp; // Dampening
-        ball.velocity[1] *= ball.bounce_damp; // Dampening
-    }
-    
-    
-    else if (ball.pos[1] > ceil_height) {
-        ball.velocity[1] *= -1; // Reflect Vertically
-        ball.pos[1] = ceil_height + .01;
-        ball.velocity[0] *= ball.bounce_damp; // Dampening
-        ball.velocity[1] *= ball.bounce_damp; // Dampening
+    int n = 0;
+    while (n < ball.size()) {
+        // Detect Collision with floor
+        if (ball[n].pos[1] < floor_height) {
+            ball[n].velocity[1] *= -1; // Reflect Vertically
+            ball[n].pos[1] = floor_height + .01;
+            ball[n].velocity[0] *= ball[n].bounce_damp; // Dampening
+            ball[n].velocity[1] *= ball[n].bounce_damp; // Dampening
+        }
+        
+        
+        else if (ball[n].pos[1] > ceil_height) {
+            ball[n].velocity[1] *= -1; // Reflect Vertically
+            ball[n].pos[1] = ceil_height + .01;
+            ball[n].velocity[0] *= ball[n].bounce_damp; // Dampening
+            ball[n].velocity[1] *= ball[n].bounce_damp; // Dampening
+        }
+        
+        n += 1;
     }
 }
 
 
 // Wall Collisions
 void wall_collision(int left, int right) {
-    
-    // Detect Collision with left wall
-    if (ball.pos[0] < left) {
-        ball.velocity[0] *= -1; // Reflect Horizontaly
-        ball.velocity[0] *= ball.bounce_damp; // Dampening
-        ball.velocity[1] *= ball.bounce_damp; // Dampening
-    }
-    
-    // Detect Collision with right
-    else if (ball.pos[0] > right){
-        ball.velocity[0] *= -1; // Reflect Horizontaly
-        ball.velocity[0] *= ball.bounce_damp; // Dampening
-        ball.velocity[1] *= ball.bounce_damp; // Dampening
+    int n = 0;
+    while (n < ball.size()) {
+        // Detect Collision with left wall
+        if (ball[n].pos[0] < left) {
+            ball[n].velocity[0] *= -1; // Reflect Horizontaly
+            ball[n].velocity[0] *= ball[n].bounce_damp; // Dampening
+            ball[n].velocity[1] *= ball[n].bounce_damp; // Dampening
+        }
+        
+        // Detect Collision with right
+        else if (ball[n].pos[0] > right){
+            ball[n].velocity[0] *= -1; // Reflect Horizontaly
+            ball[n].velocity[0] *= ball[n].bounce_damp; // Dampening
+            ball[n].velocity[1] *= ball[n].bounce_damp; // Dampening
+        }
+        
+        n += 1;
     }
 }
 
 
 
 std::vector<float> sim_operations() {
-    ball.velocity[0] *= global.air_damping;
-    ball.velocity[1] *= global.air_damping;
+    std::vector<float> output; // Declare outside the loop
+    
+    int n = 0;
+    while (n < ball.size()) {
+        ball[n].velocity[0] *= air_damping;
+        ball[n].velocity[1] *= air_damping;
 
-    
-    ball.velocity[1] += global.gravity;
-    ball.pos[0] += ball.velocity[0];
-    ball.pos[1] += ball.velocity[1];
-    
-    
-    
+        ball[n].velocity[1] += gravity;
+        ball[n].pos[0] += ball[n].velocity[0];
+        ball[n].pos[1] += ball[n].velocity[1];
+
+        // Add each ball's transformed position to the output vector
+        output.push_back((ball[n].pos[0] / sim_width) * window_width);
+        output.push_back((ball[n].pos[1] / sim_height) * window_height);
+
+        ++n;
+    }
+
     floor_ceil_collision(0, sim_height);
-    
     wall_collision(0, sim_width);
-    
-    pin_solver();
-    std::vector<float> output = {((ball.pos[0]/sim_width)*window_width),((ball.pos[1]/sim_height)*window_height)};
-    
+    pin_collisions();
+
     return output;
 }
+
 
 
 
@@ -163,13 +176,16 @@ int main() {
     initialize_pins();
     
     int count = 0;
-    while (count <= global.loop_thresh-1) {
-        std::vector<float> screen_pos = sim_operations();
-        std::cout << "\t | \t";
-        std::cout << "Position: (" << screen_pos[0] << ", " << screen_pos[1] << ") "
-                  << "Velocity: (" << ball.velocity[0] << ", " << ball.velocity[1] << ")" << std::endl;
+    while (count <= loop_thresh-1) {
+        int n = 0;
+        while (n < ball.size()) {
+            std::vector<float> screen_pos = sim_operations();
+            std::cout << "\t | \t";
+            std::cout << "Position: (" << screen_pos[0] << ", " << screen_pos[1] << ") "
+                      << "Velocity: (" << ball[n].velocity[0] << ", " << ball[n].velocity[1] << ")" << std::endl;
 
-
+            n += 1;
+        }
         count += 1;
     }
         
